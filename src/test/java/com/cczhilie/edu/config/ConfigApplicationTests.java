09fb171310c1fc51bbe585ca3d149fb465b2d2da
m15527185733@163.com <m15527185733@163.com>
2020-04-10 18:52:43 +0800

    first commit

---
+++ src/test/java/com/cczhilie/edu/config/ConfigApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cczhilie.edu.config;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
