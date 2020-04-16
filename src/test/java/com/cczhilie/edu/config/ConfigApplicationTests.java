4f7b689ec8cb1212d5cbe31e681a6e005396233a
m15527185733@163.com <m15527185733@163.com>
2020-04-16 12:03:40 +0800

    增加docker部署

--- src/test/java/com/cczhilie/edu/config/ConfigApplicationTests.java
+++ src/test/java/com/cczhilie/edu/config/ConfigApplicationTests.java
@@ -2,9 +2,12 @@ package com.cczhilie.edu.config;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class ConfigApplicationTests {
 
     @Test
